5ad7051c8e0ae033850251dd6e79e8b4369228c0
Davi Holanda <daviholandas@gmail.com>
2020-05-18 20:52:27 -0300

    Finalizing tests for while

---
+++ tests/RiseHealthCare.DomainTests/Management/ManagementFixtureTests.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using RiseHealthCare.Domain.Management;
+using RiseHealthCare.Domain.Management.Enums;
+using RiseHealthCare.Domain.Management.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RiseHealthCare.DomainTests.Management
+{
+    [CollectionDefinition(nameof(ManagementTestsCollection))]
+    public class ManagementTestsCollection : ICollectionFixture<ManagementFixtureTests> { }
+    public class ManagementFixtureTests
+    {
+        public Professional GenareteProfessionalWithOutPorcedures()
+        {
+            var professional = new Faker<Professional>("pt_BR")
+                .CustomInstantiator(p => new Professional(p.Random.Int(), p.Name.FullName(), p.Date.Between(new DateTime(2010,1,1),DateTime.Today),
+                    p.Date.Between(new DateTime(2010, 1, 1), DateTime.Today), null, null, null, p.Random.Bool()
+                ));
+
+            return professional;
+        }
+
+        public IEnumerable<Procedure> GenerateProcedures(int quantity)
+        {
+            var procedures = new Faker<Procedure>("pt_BR")
+                .CustomInstantiator(p => new Procedure(p.Random.Int(), p.Commerce.ProductName(), p.Random.Words(),p.Random.Decimal(min:1M, max:5M), p.Random.Decimal(min: 1M, max: 5M), p.Random.Enum<TypeDeduction>()));
+
+            return procedures.Generate(quantity);
+        }
+
+        public Procedure GenareteProcedure(decimal price, decimal deduction, TypeDeduction typeDeduction)
+        {
+            var procedures = new Faker<Procedure>("pt_BR")
+                .CustomInstantiator(p => new Procedure(p.Random.Int(), p.Commerce.ProductName(), p.Random.Words(), price, deduction, typeDeduction));
+
+            return procedures;
+        }
+
+        public Professional GenareteProfessionalWithOutHiringAndFiringDate(DateTime hiringDate, DateTime firingDate)
+        {
+            var professional = new Faker<Professional>("pt_BR")
+                .CustomInstantiator(p => new Professional(p.Random.Int(), p.Name.FullName(), hiringDate,
+                    firingDate, null, null, null, p.Random.Bool()
+                ));
+
+            return professional;
+        }
+    }
+}
---
+++ tests/RiseHealthCare.DomainTests/Management/ManagementTests.cs
@@ -0,0 +1,70 @@
+﻿using Bogus.DataSets;
+using FluentAssertions;
+using RiseHealthCare.Domain.Management.Enums;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xunit;
+
+namespace RiseHealthCare.DomainTests.Management
+{
+    [Collection(nameof(ManagementTestsCollection))]
+    public class ManagementTests
+    {
+        private readonly ManagementFixtureTests _managementFixture;
+
+        public ManagementTests(ManagementFixtureTests managementFixture)
+        {
+            _managementFixture = managementFixture;
+        }
+
+
+        [Theory(DisplayName = "Testing calculation comission per procedure")]
+        [InlineData(120, 30, 90, TypeDeduction.Fixed)]
+        [InlineData(250, 25.5, 186.25, TypeDeduction.Percent)]
+        [InlineData(20, 25.5, 20, TypeDeduction.Percent)]
+        public void Procedure_CalculationComission_CorrectCalculation(decimal price, decimal deduction, decimal comission, TypeDeduction typeDeduction)
+        {
+            //Arrange
+            var procedure = _managementFixture.GenareteProcedure(price, deduction, typeDeduction);
+            //Act
+            var resultComission = procedure.CalculateComission();
+            //Assert
+            resultComission.Should().Equals(comission);
+        }
+
+        [Fact(DisplayName = "Testing add range procedure for professional")]
+        public void Professional_AddProcedure_AddRangeProcedure()
+        {
+            //Arrange
+            var procedures = _managementFixture.GenerateProcedures(10);
+            var professional = _managementFixture.GenareteProfessionalWithOutPorcedures();
+            //Act
+            foreach (var procedure in procedures)
+            {
+                professional.AddProcedure(procedure);
+            }
+            //Assert
+            professional.Procedures.Should().HaveCount(10);
+        }
+
+        [Fact(DisplayName = "Testing calculation time of work for professional")]
+        public void Profissional_CalculationTimeOfWork()
+        {
+            //Arrange
+            var professional1 = _managementFixture.GenareteProfessionalWithOutHiringAndFiringDate(new DateTime(2010,05,01), new DateTime(2019,10,02));
+            var professional2 = _managementFixture.GenareteProfessionalWithOutHiringAndFiringDate(new DateTime(2002, 11, 20), DateTime.Now);
+            
+            //Act
+            var time1 = professional1.TimeOfWork();
+            var time2 = professional2.TimeOfWork();
+            
+            //Assert
+            time1.Should().BeEquivalentTo((9, 5, 1));
+            time2.Should().BeEquivalentTo((17,5,29));
+
+        }
+
+    }
+}
